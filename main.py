232e174d988957905c1c0a6770459e2eb9c6609e
MYR <loic.themyr@gmail.com>
2020-11-19 10:50:55 +0100

    data

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 def main():
-	os.system("python convert_dataset_to_npy.py --config_file datasets/configs/tcia_slices.yaml --data_dir /local/HD_DEEPLEARNING/TCIA_pancreas/ --image_output_dir ~/data/TCIA_torch/images/ --annotation_output_dir ~/data/TCIA_torch/labels/ --save_mode volumes")
+	os.system("python convert_dataset_to_npy.py --config_file datasets/configs/tcia_slices.yaml --data_dir /local/HD_DEEPLEARNING/TCIA_pancreas/ --image_output_dir /local/DEEPLEARNING/pancreas_multi_resolution/images/ --annotation_output_dir /local/DEEPLEARNING/pancreas_multi_resolution/labels/ --save_mode volumes")
 	#os.system("python convert_dataset_to_npy.py --config_file datasets/configs/tcia_slices.yaml --data_dir ~/datasets/ --image_output_dir ~/datasets/first_for_labels/images/ --annotation_output_dir ~/datasets/first_for_labels/labels/ --save_mode volumes")
 
 
